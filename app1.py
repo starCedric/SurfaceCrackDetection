45230956e0bfb41a8aa60cc991eb71aa87206fe4
Sandeep Kumar Dubey <143398446+cedricodes@users.noreply.github.com>
2024-03-21 12:24:49 +0530

    Loading the model from drive

--- app1.py
+++ app1.py
@@ -5,9 +5,32 @@ from tensorflow.keras.models import load_model
 from PIL import Image
 import numpy as np
 import time
+import requests
+from io import BytesIO
+
+model_url = 'https://drive.google.com/file/d/1UOj5kZB2Nv5R_UNn9_09cundJH8qT0Wv/view?usp=sharing'
+
+# Function to download the model file from Google Drive and save it locally
+@st.cache_resource
+def download_model(url, destination):
+    response = requests.get(url)
+    with open(destination, 'wb') as f:
+        f.write(response.content)
+
+# Download the model file from Google Drive
+model_filename = 'my_CNN.h5'
+download_model(model_url, model_filename)
+
+# Load the trained model from the downloaded file
+@st.cache_resource
+def load_remote_model():
+    model = load_model(model_filename)
+    return model
+
+model = load_remote_model()
 
 # Load the trained model
-model = load_model('https://drive.google.com/file/d/1UOj5kZB2Nv5R_UNn9_09cundJH8qT0Wv/view?usp=sharing')
+#model = load_model('https://drive.google.com/file/d/1UOj5kZB2Nv5R_UNn9_09cundJH8qT0Wv/view?usp=sharing')
 
 def preprocess_image(image_file):
     img = Image.open(image_file)
@@ -33,7 +56,7 @@ def main():
         if prediction[0][0] >= 0.5:
             st.write("Surface type : Cracked Surface")
         else:
-            st.write("Surface type : Smooth Surface")
+            st.write("Surface type : No Cracks Found")
 
     st.caption("Project completed by Sandeep")    
 
